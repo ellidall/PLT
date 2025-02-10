41ba585372744ba277bb6dfc55f44be8724715e4
Gregogo <grigoriy.poshnagov@ispring.institute>
2025-02-11 00:21:46 +0300

    Добавил обработчик ошибок

---
+++ errorHandler.ts
@@ -0,0 +1,36 @@
+import {ErrorCode, errorMessages} from "./errorMessages";
+import {Token} from "./globalTypes";
+
+class ErrorHandler {
+    private errors: string[] = [];
+
+    addError(errorCode: ErrorCode, token?: Token) {
+        let message = errorMessages[errorCode];
+
+        if (token && message.includes('{token}')) {
+            message = message.replace(/{token}/g, token.lexeme);
+        }
+
+        const errorLocation = token
+            ? `Line ${token.position.line}, column ${token.position.column}: ${message}`
+            : message;
+
+        this.errors.push(errorLocation);
+    }
+
+    hasErrors() {
+        return this.errors.length > 0;
+    }
+
+    getErrors() {
+        return this.errors;
+    }
+
+    clearErrors() {
+        this.errors = [];
+    }
+}
+
+export {
+    ErrorHandler,
+}
---
+++ errorMessages.ts
@@ -0,0 +1,29 @@
+enum ErrorCode {
+    E001 =  'E001',
+    E002 = 'E002',
+    E003 = 'E003',
+    E004 = 'E004',
+    E005 = 'E005',
+    E006 = 'E006',
+    E007 = 'E007',
+    E008 = 'E008',
+}
+
+const errorMessages = {
+    'E001': "Unclosed block comment",
+    'E002': "Invalid number: multiple dots",
+    'E003': "Invalid exponent format",
+    'E004': "Invalid characters after number",
+    'E005': "Cyrillic letters in identifier",
+    'E006': "Unclosed string literal",
+    'E007': "Invalid operator/symbol '{token}'",
+    'E008': "Unexpected end of float number",
+
+    // Общая ошибка для непредвиденных случаев
+    'E000': "Unrecognized token '{token}'"
+}
+
+export {
+    ErrorCode,
+    errorMessages
+}
